e331b5f72b0b393ce014e525def1408cbf6a7371
bkmcpherson <56423600+bkmcpherson@users.noreply.github.com>
2019-12-15 20:34:24 -0600

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,3 +1,4 @@
+library("data.table")
 df <- data.table::fread("household_power_consumption.txt", na.strings = "?") #read data into df variable
 library("lubridate")
 df$Date <- lubridate::dmy(df$Date) #convert "Date" column to date class
@@ -5,5 +6,5 @@ df <- subset(df, df$Date == "2007-02-01" | df$Date == "2007-02-02") #Subset data
 df[, dateTime := as.POSIXct(paste(Date, Time), format = "%Y-%m-%d %H:%M:%S")] #create new column with date & time combined
 
 png("plot2.png", width = 480, height = 480) #Initialize png graphic device with height/width of 480 pixels
-plot(df$Global_active_power ~ df$dateTime, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "") #create plot area without showing points
+plot(df$Global_active_power ~ df$dateTime, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "") #create plot using lines
 dev.off() #close graphic device
