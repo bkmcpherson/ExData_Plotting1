9fa0bb310b249c88252bb3a299618ac434223811
bkmcpherson <56423600+bkmcpherson@users.noreply.github.com>
2019-12-15 20:35:46 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,4 @@
+library("data.table")
 df <- data.table::fread("household_power_consumption.txt", na.strings = "?") #read data into df variable
 library("lubridate")
 df$Date <- lubridate::dmy(df$Date) #convert "Date" column to date class
@@ -5,8 +6,8 @@ df <- subset(df, df$Date == "2007-02-01" | df$Date == "2007-02-02") #Subset data
 df[, dateTime := as.POSIXct(paste(Date, Time), format = "%Y-%m-%d %H:%M:%S")] #create new column with date & time combined
 
 png("plot3.png", width = 480, height = 480) #Initialize png graphic device with height/width of 480 pixels
-plot(df$Sub_metering_1 ~ df$dateTime, type = "l", ylab = "Energy sub metering", xlab = "") #create plot area without showing points
-lines(df$Sub_metering_2 ~ df$dateTime, col = "red")
-lines(df$Sub_metering_3 ~ df$dateTime, col = "blue")
-legend("topright", col = c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1), lwd = c(1,1))
+plot(df$Sub_metering_1 ~ df$dateTime, type = "l", ylab = "Energy sub metering", xlab = "") #create plot area
+lines(df$Sub_metering_2 ~ df$dateTime, col = "red") #add submeter2 red lines
+lines(df$Sub_metering_3 ~ df$dateTime, col = "blue") #add submeter3 blue lines
+legend("topright", col = c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1), lwd = c(1,1)) #create legend
 dev.off() #close graphic device
